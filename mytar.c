f7dd4037710546ee8bccfebbf216499650e49347
devyanshuk <devkoirala10@gmail.com>
2020-07-31 14:02:35 +0200

    updates

--- mytar.c
+++ mytar.c
@@ -58,9 +58,37 @@ void writeToFile(long file_size, char *buffer, int position, int blockSize, char
 			break;
 		}
 
-		fprintf(fp, "%c", buffer[position + blockSize + i]);
+		fputc(buffer[position + blockSize + i], fp);
 	}
 	fclose(fp);
+}
+
+void searchForBlocks(int position, int fileOffset, int blockSize, char *buffer, int index, int argc, char **searchNames){
+	int zero1Position = position + fileOffset;
+	int zero2Position = zero1Position + blockSize;
+
+	// First Zero block check
+	int zero1Chksum = calculateCheckSum(blockSize, zero1Position, buffer);
+	int zero2Chksum = calculateCheckSum(blockSize, zero2Position, buffer);
+
+	if (zero1Chksum == blockSize || tarSize - zero1Position == blockSize)
+		printf("mytar: A lone zero block at %d\n", 1 + zero1Position/blockSize);
+
+	bool found = false;
+	for (int i = index; i < argc; i++){
+		if (strcmp(searchNames[i], " ") != 0){
+			fprintf(stderr, "%s: %s: %s\n", "mytar", searchNames[i], "Not found in archive");
+			fflush(stdout);
+			found = true;
+		}
+	}
+	if (tarSize-zero1Position < 0 && zero2Chksum != blockSize && zero1Chksum != blockSize) {
+		fflush(stdout);
+		fprintf(stderr, "%s", "mytar: Unexpected EOF in archive\n");
+		errx(2, "Error is not recoverable: exiting now");
+	} 
+	if (found)
+		errx(2, "Exiting with failure status due to previous errors");
 }
 
 void printTarFiles(char *buffer, char** searchNames, int index, int argc, bool verbose, bool extract, bool tOptionFound){
@@ -71,10 +99,8 @@ void printTarFiles(char *buffer, char** searchNames, int index, int argc, bool v
 	if (extract || tOptionFound){
 		do{
 			position += fileOffset;
-
 			tarHeader* header;
 			header = (tarHeader*)(buffer+position);
-
 			// Since size is in octal, we need to convert to decimal
 			char *ptr;
 			long file_size;
@@ -82,10 +108,8 @@ void printTarFiles(char *buffer, char** searchNames, int index, int argc, bool v
 
 			fileOffset = (1 + file_size/blockSize + (file_size % blockSize != 0)) * blockSize;
 
-			if (strcmp(header->chksum, "") == 0){
+			if (strcmp(header->chksum, "") == 0)
 				break;
-			}
-
 			if (header->typeflag != '0' && header->typeflag!='\x00')
 				errx(2, "%s%d", "Unsupported header type: ", header->typeflag);
 
@@ -104,66 +128,34 @@ void printTarFiles(char *buffer, char** searchNames, int index, int argc, bool v
 							printf("%s\n", header->name);
 							fflush(stdout);
 						}
-						if (extract){
+						if (extract)
 							writeToFile(file_size, buffer, position, blockSize, header->name);
-						}
 						searchNames[c] = " ";
 						break;
 					}
 					c++;
 				}
 			}
-
-			if (position + fileOffset + 2 * blockSize >= tarSize){
+			if (position + fileOffset + 2 * blockSize >= tarSize)
 				break;
-			}
-
 		} while(position + fileOffset + blockSize + 2 * blockSize <= tarSize);
 	}
-
-	int zero1Position = position + fileOffset;
-	int zero2Position = zero1Position + blockSize;
-
-	// First Zero block check
-	int zero1Chksum = calculateCheckSum(blockSize, zero1Position, buffer);
-
-	int zero2Chksum = calculateCheckSum(blockSize, zero2Position, buffer);
-
-	if (zero1Chksum == blockSize || tarSize - zero1Position == blockSize){
-		printf("mytar: A lone zero block at %d\n", 1 + zero1Position/blockSize);
-	}
-
-		bool found = false;
-
-		for (int i = index; i < argc; i++){
-			if (strcmp(searchNames[i], " ") != 0){
-				fprintf(stderr, "%s: %s: %s\n", "mytar", searchNames[i], "Not found in archive");
-				fflush(stdout);
-				found = true;
-			}
-		}
-
-		if (tarSize-zero1Position < 0 && zero2Chksum != blockSize && zero1Chksum != blockSize) {
-			fflush(stdout);
-			fprintf(stderr, "%s", "mytar: Unexpected EOF in archive\n");
-			errx(2, "Error is not recoverable: exiting now");
-	    }
-
-		if (found)
-			errx(2, "Exiting with failure status due to previous errors");
+	searchForBlocks(position, fileOffset, blockSize, buffer, index, argc, searchNames);
 }
 
 char* openTarFile(char *file){
 	FILE *tarFile;
 	tarFile = fopen(file , "rb" );
-	if (!tarFile)
+	if (tarFile == NULL)
 		errx(2, "%s '%s'","Error opening archive: Failed to open", file);
 	fseek(tarFile, 0, SEEK_END);
 	tarSize = ftell(tarFile);
 	fseek(tarFile, 0, SEEK_SET);
 	char *buffer;
 	buffer = (char*) malloc(tarSize);
-	fread(buffer,sizeof(char),tarSize,tarFile);
+	//printf("tarsize = %ld and size_t tarsize = %ld\n", tarSize, (size_t)tarSize);
+	if (fread(buffer,sizeof(char),tarSize,tarFile) != (size_t)tarSize)
+		errx(2, "Unexpected EOF in archive\nmytar: Error is not recoverable: exiting now");
 	tarHeader* header;
 	header = (tarHeader*)(buffer);
 	if (strcmp(header->magic, "ustar  ") != 0)
